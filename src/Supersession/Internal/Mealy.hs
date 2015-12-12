2d590d862ca4b2e20a74310c3be1b5316fee44ca
Julian K. Arni <jkarni@gmail.com>
2015-12-12 18:22:43 +0100

    polymorphic and constraint-polymorphic input and output

--- src/Supersession/Internal/Mealy.hs
+++ src/Supersession/Internal/Mealy.hs
@@ -1,27 +1,43 @@
+{-# LANGUAGE RankNTypes #-}
 module Supersession.Internal.Mealy where
 
-import Control.Monad.State
-import Control.Monad
+import Control.Arrow
 import GHC.Exts
 
-next :: (Monad m, MealyStep sing a) => sing a -> Mealy m
-next sing = Mealy (move sing . read)
+next :: (Monad m, MealyStep sing a out orig final) => sing a out -> Mealy m orig final
+next sing = Mealy go
+  where go o = first render <$> move sing (parse o)
 
 
-data Mealy (m :: * -> *) = Mealy {  unMealy :: String -> m (String, Mealy m) }
+data Mealy (m :: * -> *) orig final = Mealy {
+    unMealy :: orig -> m (final, Mealy m orig final)
+    }
 
 
+class (Parse orig a, Render out final) => MealyStep sing a out orig final where
+    move :: Monad m => sing a out -> a -> m (out, Mealy m orig final)
 
-class Read a => MealyStep sing a where
-    move :: Monad m => sing a -> a -> m (String, Mealy m)
 
-
-runMealy' :: (Monad m) => Mealy m -> [String] -> m [String]
+runMealy' :: (Monad m) => Mealy m orig final -> [orig] -> m [final]
 runMealy' _ []             = return []
 runMealy' (Mealy m) (x:xs) =  m x >>= \(o, m') -> ( o :) <$> runMealy' m' xs
 
-runMealy :: (Monad m, MealyStep sing start) => sing start -> [String] -> m [String]
-runMealy s = runMealy' (Mealy (move s . read))
+runMealy :: (Monad m, MealyStep sing start out orig final)
+         => sing start out -> [orig] -> m [final]
+runMealy s = runMealy' (Mealy go)
+  where go o = first render <$> move s (parse o)
+
+class Parse from to where
+    parse :: from -> to
+
+class Render from to where
+    render :: from -> to
+
+--- Parsing and Rendering
+instance Read a => Parse String a where
+    parse = read
+instance Show a => Render a String where
+    render = show
 
 type family Elem a as :: Constraint where
     Elem a (a ': as) = ()
--- test/SupersessionSpec.hs
+++ test/SupersessionSpec.hs
@@ -1,4 +1,5 @@
 {-# LANGUAGE RankNTypes #-}
+{-# LANGUAGE ConstraintKinds #-}
 module SupersessionSpec (spec) where
 
 import Test.Hspec
@@ -9,9 +10,9 @@ spec :: Spec
 spec = describe "it" $ do
     it "should work" $ do
         let test = ["Login \"jkarni\"", "Msg \"hi\"", "Logout"]
-        runMealy LoggedOutSing test `shouldBe` Identity [ "now logged in as jkarni"
-                                               , "jkarni says: hi"
-                                               , "now logged out!"
+        runMealy LoggedOutSing test `shouldBe` Identity [ "()"
+                                               , "\"jkarni says: hi\""
+                                               , "\"now logged out!\""
                                                ]
 
 ----Logged in ----------------------------------------------------------------
@@ -19,11 +20,15 @@ spec = describe "it" $ do
 data LoggedIn = Msg String | Logout
     deriving (Eq, Show, Read)
 
-loggedIn :: Monad m => EgSing LoggedIn -> LoggedIn -> m (String, Mealy m)
+loggedIn :: (NeededCtx orig final, Monad m)
+         => EgSing LoggedIn String
+         -> LoggedIn
+         -> m (String, Mealy m orig final)
 loggedIn _ Logout  = return ("now logged out!", next LoggedOutSing)
-loggedIn (LoggedInSing usr) (Msg m) = return (usr ++ " says: " ++ m, next $ LoggedInSing usr)
+loggedIn (LoggedInSing usr) (Msg m)
+    = return (usr ++ " says: " ++ m, next $ LoggedInSing usr)
 
-instance MealyStep EgSing LoggedIn where
+instance (NeededCtx orig final) => MealyStep EgSing LoggedIn String orig final where
     move = loggedIn
 
 -----Logged out --------------------------------------------------------------
@@ -31,18 +36,28 @@ instance MealyStep EgSing LoggedIn where
 data LoggedOut = Login Username | Error
     deriving (Eq, Show, Read)
 
-loggedOut :: Monad m => LoggedOut -> m (String, Mealy m)
-loggedOut (Login usr) = return ("now logged in as " ++ usr, next $ LoggedInSing usr)
-loggedOut Error = return ("messages not allowed", next LoggedOutSing)
+loggedOut :: (NeededCtx orig final, Monad m)
+          => LoggedOut -> m ((), Mealy m orig final)
+loggedOut (Login usr) = return ((), next $ LoggedInSing usr)
+loggedOut Error = return ((), next LoggedOutSing)
 
-instance MealyStep EgSing LoggedOut where
+instance ( NeededCtx orig final)
+        => MealyStep EgSing LoggedOut () orig final where
     move _ = loggedOut
 
 -----General ------------------------------------------------------------------
 type Username = String
 
 -- Contains constructors for all possible states
-data EgSing a where
-    LoggedOutSing :: EgSing LoggedOut
-    LoggedInSing  :: Username -> EgSing LoggedIn
+data EgSing a b where
+                --         input    output
+                --          vvv      vvv
+    LoggedOutSing :: EgSing LoggedOut ()
+    LoggedInSing  :: Username -> EgSing LoggedIn String
     -- ^ The singleton may be used to keep state
+
+-- This includes all constraints we need. Only necessary if we choose to leave
+-- orig and final polymorphic
+type NeededCtx orig final = ( Parse orig LoggedOut, Parse orig LoggedIn
+                            , Render () final, Render String final
+                            )
